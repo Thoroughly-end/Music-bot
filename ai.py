101e64c47fcb0f7064597179111d7d4b1c49fa0c
Thoroughly-end <allenkuo0818@gmail.com>
2025-04-29 16:26:28 +0800

    switch to slash commands, and add ai mode

---
+++ ai.py
@@ -0,0 +1,41 @@
+from openai import OpenAI
+import json
+from config import config
+import asyncio
+
+def ai_get_songs(song):
+    content = "I like to listen "+song+".What songs I am likely to love?"
+    client = OpenAI(api_key = config.OPENAI_API_KEY)
+    response = client.responses.create(
+    model="gpt-4.1",
+    input=[{"role": "user", "content": content}],
+    text={
+            "format": {
+                "type": "json_schema",
+                "name": "songs",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "songs": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "title": {"type": "string"},
+                                    "artist": {"type": "string"}
+                                },
+                                "required": ["title", "artist"],
+                                "additionalProperties": False
+                            }
+                        },
+                    },
+                    "required": ["songs"],
+                    "additionalProperties": False
+                },
+                "strict": True
+            }
+        }
+    )
+    songs_list = json.loads(response.output_text)['songs']
+    return songs_list
+
